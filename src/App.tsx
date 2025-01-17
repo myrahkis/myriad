aff013a5e0401296c282511c4dc79c008c02bb1b
myrahkis <kruzinka@yandex.ru>
2025-01-17 17:21:35 +0300

    infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import { getAllPosts } from "./services/postsApi";
+
+type Post = {
+  body: string;
+  id: number;
+  reactions: object;
+  tags: string[];
+  title: string;
+  userId: number;
+  views: number;
+};
+
+const MAX_PAGES = 25;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const observerRef = useRef<HTMLDivElement>(null);
+  const isFetching = useRef(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      if (isFetching.current || page > MAX_PAGES) return;
+
+      try {
+        setIsLoading(true);
+        isFetching.current = true;
+
+        const newPosts = await getAllPosts(page);
+
+        // console.log(page);
+        setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+        setError("");
+      } catch (err) {
+        if (err instanceof Error) {
+          console.error(err.message);
+          setError(err.message);
+        } else {
+          setError("Произошла неизвестная ошибка");
+        }
+      } finally {
+        setIsLoading(false);
+        isFetching.current = false;
+      }
+    }
+
+    fetchPosts();
+  }, [page]);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      const [entry] = entries;
+
+      if (entry.isIntersecting && !isFetching.current) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    const ref = observerRef.current;
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id} style={{ padding: "1.5rem" }}>
+            {post.id}. {post.title}
+          </li>
+        ))}
+      </ul>
+      <div ref={observerRef}></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/postsApi.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const url = "https://dummyjson.com/posts";
+const limitPerPage = 10;
+
+type Post = {
+  body: string;
+  id: number;
+  reactions: object;
+  tags: string[];
+  title: string;
+  userId: number;
+  views: number;
+};
+
+export async function getAllPosts(page: number): Promise<Post[]> {
+  const res = await axios.get(
+    `${url}?limit=${limitPerPage}&skip=${page * limitPerPage}`
+  );
+
+  if (res.statusText !== "OK") throw new Error("Couldn't fetch all posts");
+
+  console.log(res.data.posts);
+
+  return res.data.posts;
+}
